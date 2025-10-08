fd6485a890246da7ceaf19e4e5ea92d4cf2067ad
dyakubovskiy <dmitriy@yakubovskuy.ru>
2025-10-08 21:28:11 +0300

    Внес правки для tree-shaking

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,42 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const analyze = mode === 'analyze'
+
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+      ...(analyze
+        ? [
+            visualizer({
+              filename: 'dist/bundle-report.html',
+              template: 'treemap',
+              gzipSize: true,
+              brotliSize: true,
+            }),
+          ]
+        : []),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    build: {
+      rollupOptions: {
+        treeshake: {
+          moduleSideEffects: false,
+          propertyReadSideEffects: false,
+          tryCatchDeoptimization: false,
+        },
+      },
+    },
+  }
 })
