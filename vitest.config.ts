0164bc23a5c9dbb4c4795e418b8b286928c167a2
dyakubovskiy <dmitriy@yakubovskuy.ru>
2025-10-09 18:31:31 +0300

    Изменил файл для анализа покрытия тестов

--- vitest.config.ts
+++ vitest.config.ts
@@ -13,6 +13,20 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      coverage: {
+        exclude: [
+          'playwright.config.ts',
+          'vite.config.ts',
+          'vitest.config.ts',
+          'eslint.config.ts',
+          'env.d.ts',
+          'dist/**',
+          'coverage/**',
+          'src/main.ts',
+          'src/app/**',
+          'src/**/index.ts'
+        ]
+      }
     },
   }),
 )
