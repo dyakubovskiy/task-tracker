11e1b15eae8a1f8d782aa28fc14c0e5ef9092371
dyakubovskiy <dmitriy@yakubovskuy.ru>
2025-10-09 16:49:13 +0300

    Убрал devtools из prod сборки

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, type PluginOption } from 'vite'
 import { visualizer } from 'rollup-plugin-visualizer'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -8,35 +8,40 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const analyze = mode === 'analyze'
+  const isDev = mode === 'development'
+
+  const plugins: Array<PluginOption> = [vue()]
+
+  if (isDev) {
+    const devtools = vueDevTools()
+    if (devtools) plugins.push(devtools)
+  }
+
+  if (analyze)
+    plugins.push(
+      visualizer({
+        filename: 'dist/bundle-report.html',
+        template: 'treemap',
+        gzipSize: true,
+        brotliSize: true
+      })
+    )
 
   return {
-    plugins: [
-      vue(),
-      vueDevTools(),
-      ...(analyze
-        ? [
-            visualizer({
-              filename: 'dist/bundle-report.html',
-              template: 'treemap',
-              gzipSize: true,
-              brotliSize: true,
-            }),
-          ]
-        : []),
-    ],
+    plugins,
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-      },
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     },
     build: {
       rollupOptions: {
         treeshake: {
           moduleSideEffects: false,
           propertyReadSideEffects: false,
-          tryCatchDeoptimization: false,
-        },
-      },
-    },
+          tryCatchDeoptimization: false
+        }
+      }
+    }
   }
 })
