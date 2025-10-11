d17c4a4d758cca720667e5737f7f44ef768e14cd
dyakubovskiy <dmitriy@yakubovskuy.ru>
2025-10-12 00:14:40 +0300

    Добавил VButton в public api

--- src/shared/ui/button/index.ts
+++ src/shared/ui/button/index.ts
@@ -1,7 +1,8 @@
 import type { ButtonIconProps } from './VButtonIcon.vue'
 
 import VButtonIcon from './VButtonIcon.vue'
+import VButton from './VButton.vue'
 
 export type { ButtonIconProps }
 
-export { VButtonIcon }
+export { VButtonIcon, VButton }
